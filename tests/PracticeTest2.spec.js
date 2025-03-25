8c43e7ead6cf742e06a1e1f1ca7aa461f77e8670
Milos Nesic <milos.nesic@vegait.rs>
2025-03-25 12:45:44 +0100

    Deleting unnecessary tests

--- tests/PracticeTest2.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-
-test('First Playwright Test', async ({ page }) => 
-{
-    await page.goto("https://practicesoftwaretesting.com/");
-    console.log(await page.locator(".card-title").nth(0));
-
-});
